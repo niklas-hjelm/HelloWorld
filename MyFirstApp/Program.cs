4f860e1bc54b7093e7af7308719d8445cb392164
niklas-hjelm <88381305+niklas-hjelm@users.noreply.github.com>
2021-08-10 10:08:45 +0200

    Inlämning

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! My name is Niklas!");
+        }
+    }
+}
